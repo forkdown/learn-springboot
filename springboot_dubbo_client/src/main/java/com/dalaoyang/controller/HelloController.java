6a91766929ab6a73c99ca5acda11468bda354d18
397600342@qq.com <a8320105>
2018-06-14 10:27:37 +0800

    springboot_dubbo

---
+++ springboot_dubbo_client/src/main/java/com/dalaoyang/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.dalaoyang.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.dalaoyang.dubbo.HelloConsumerService;
+import com.dalaoyang.dubbo.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dalaoyang
+ * @Description
+ * @project springboot_learn
+ * @package com.dalaoyang.controller
+ * @email yangyang@dalaoyang.cn
+ * @date 2018/6/14
+ */
+@RestController
+public class HelloController {
+
+    @Reference(version = "1.0.0")
+    HelloService helloService;
+
+    @GetMapping("sayHello")
+    public String sayHello(String name){
+        return helloService.SayHello(name);
+    }
+}
