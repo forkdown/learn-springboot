93470171708d4407a980c4eb58aa9d890b4c20bb
397600342@qq.com <a8320105>
2018-06-22 16:42:11 +0800

    springboot

--- springboot_dubbo_client/src/main/java/com/dalaoyang/controller/HelloController.java
+++ springboot_dubbo_client/src/main/java/com/dalaoyang/controller/HelloController.java
@@ -1,9 +1,7 @@
 package com.dalaoyang.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.dalaoyang.dubbo.HelloConsumerService;
 import com.dalaoyang.dubbo.HelloService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
