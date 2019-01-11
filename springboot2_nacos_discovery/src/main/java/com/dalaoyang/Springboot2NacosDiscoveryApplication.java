537ced2bb6c70f5064c262d009326be8c33fc260
yangyang <a8320105>
2019-01-11 18:29:26 +0800

    springboot2_nacos

---
+++ springboot2_nacos_discovery/src/main/java/com/dalaoyang/Springboot2NacosDiscoveryApplication.java
@@ -0,0 +1,36 @@
+package com.dalaoyang;
+
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+//curl -X PUT 'http://127.0.0.1:8848/nacos/v1/ns/instance?serviceName=springboot2-nacos-discovery&ip=127.0.0.1&port=8080'
+@SpringBootApplication
+@RestController
+public class Springboot2NacosDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2NacosDiscoveryApplication.class, args);
+	}
+
+	@NacosInjected
+	private NamingService namingService;
+
+	@RequestMapping(value = "/get", method = GET)
+	@ResponseBody
+	public List<Instance> get(@RequestParam String serviceName) throws NacosException {
+		return namingService.getAllInstances(serviceName);
+	}
+}
+
