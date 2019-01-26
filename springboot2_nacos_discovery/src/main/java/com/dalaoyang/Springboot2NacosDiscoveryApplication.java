238d514946c5c3b4a177a50a842f55ac4fb2e079
yangyang <a8320105>
2019-01-26 19:07:54 +0800

    springboot2_nacos

--- springboot2_nacos_discovery/src/main/java/com/dalaoyang/Springboot2NacosDiscoveryApplication.java
+++ springboot2_nacos_discovery/src/main/java/com/dalaoyang/Springboot2NacosDiscoveryApplication.java
@@ -4,6 +4,7 @@ import com.alibaba.nacos.api.annotation.NacosInjected;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.NamingService;
 import com.alibaba.nacos.api.naming.pojo.Instance;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -20,17 +23,28 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RestController
 public class Springboot2NacosDiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Springboot2NacosDiscoveryApplication.class, args);
-	}
-
 	@NacosInjected
 	private NamingService namingService;
 
-	@RequestMapping(value = "/get", method = GET)
+	@Value("${server.port}")
+	private int serverPort;
+
+	@Value("${spring.application.name}")
+	private String applicationName;
+
+	@PostConstruct
+	public void registerInstance() throws NacosException{
+		namingService.registerInstance(applicationName,"127.0.0.1",serverPort);
+	}
+
+	@RequestMapping(value = "/getInstance", method = GET)
 	@ResponseBody
-	public List<Instance> get(@RequestParam String serviceName) throws NacosException {
+	public List<Instance> getInstance(@RequestParam String serviceName) throws NacosException {
 		return namingService.getAllInstances(serviceName);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2NacosDiscoveryApplication.class, args);
 	}
 }
 
