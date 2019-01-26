02386ece4cf1140864981d4af54157b54b594ea2
yangyang <a8320105>
2019-01-26 19:08:15 +0800

    springboot2_nacos

--- springboot2_nacos_discovery/src/main/java/com/dalaoyang/Springboot2NacosDiscoveryApplication.java
+++ springboot2_nacos_discovery/src/main/java/com/dalaoyang/Springboot2NacosDiscoveryApplication.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
-import java.net.InetAddress;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
