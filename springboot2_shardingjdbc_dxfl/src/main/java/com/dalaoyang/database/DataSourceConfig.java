159332febb18db315361911d9880982ef5973b3b
yangyang <a8320105>
2019-02-12 11:43:00 +0800

    springboot_shardingjdbc_dxfl

--- springboot2_shardingjdbc_dxfl/src/main/java/com/dalaoyang/database/DataSourceConfig.java
+++ springboot2_shardingjdbc_dxfl/src/main/java/com/dalaoyang/database/DataSourceConfig.java
@@ -44,14 +44,14 @@ public class DataSourceConfig {
     }
 
     private DataSource buildDataSource() throws SQLException {
-        //分库设置
-        Map<String, DataSource> dataSourceMap = new HashMap<>();
-        dataSourceMap.put(database1Config.getDatabaseName(), database1Config.createDataSource());
-        dataSourceMap.put(database2Config.getDatabaseName(), database2Config.createDataSource());
+        //设置从库数据源集合
+        Map<String, DataSource> slaveDataSourceMap = new HashMap<>();
+        slaveDataSourceMap.put(database1Config.getDatabaseName(), database1Config.createDataSource());
+        slaveDataSourceMap.put(database2Config.getDatabaseName(), database2Config.createDataSource());
 
-        // 获取数据源对象
-        DataSource dataSource = MasterSlaveDataSourceFactory.createDataSource("ms",database0Config.getDatabaseName()
-                ,database0Config.createDataSource(), dataSourceMap, MasterSlaveLoadBalanceStrategyType.getDefaultStrategyType());
+        //获取数据源对象
+        DataSource dataSource = MasterSlaveDataSourceFactory.createDataSource("masterSlave",database0Config.getDatabaseName()
+                ,database0Config.createDataSource(), slaveDataSourceMap, MasterSlaveLoadBalanceStrategyType.getDefaultStrategyType());
         return dataSource;
     }
 
