1e822aa8591e6b9545aae81acab4907f5d8b0a15
yangyang <dalaoyang@aliyun.com>
2019-03-05 22:47:16 +0800

    springboot2_elasticjob

--- springboot2_elasticjob/src/main/java/com/dalaoyang/config/MyJobConfig.java
+++ springboot2_elasticjob/src/main/java/com/dalaoyang/config/MyJobConfig.java
@@ -15,10 +15,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyJobConfig {
 
-
     private final String cron = "0/5 * * * * ?";
     private final int shardingTotalCount = 3;
-    private final String shardingItemParameters = "0=beijing,1=shanghai,2=hangzhou";
+    private final String shardingItemParameters = "0=A,1=B,2=C";
+    private final String jobParameters = "parameter";
 
     @Autowired
     private ZookeeperRegistryCenter regCenter;
@@ -31,25 +31,22 @@ public class MyJobConfig {
     @Bean(initMethod = "init")
     public JobScheduler simpleJobScheduler(final SimpleJob simpleJob) {
         return new SpringJobScheduler(simpleJob, regCenter, getLiteJobConfiguration(simpleJob.getClass(),
-                cron, shardingTotalCount, shardingItemParameters));
+                cron, shardingTotalCount, shardingItemParameters, jobParameters));
     }
 
     private LiteJobConfiguration getLiteJobConfiguration(final Class<? extends SimpleJob> jobClass,
                                                          final String cron,
                                                          final int shardingTotalCount,
-                                                         final String shardingItemParameters) {
-        return LiteJobConfiguration
-                .newBuilder(
-                        new SimpleJobConfiguration(
-                                JobCoreConfiguration.newBuilder(
-                                        jobClass.getName(), cron, shardingTotalCount)
-                                        .shardingItemParameters(shardingItemParameters)
-                                        .build()
-                                , jobClass.getCanonicalName()
-                        )
-                )
-                .overwrite(true)
-                .build();
+                                                         final String shardingItemParameters,
+                                                         final String jobParameters) {
+        // 定义作业核心配置
+        JobCoreConfiguration simpleCoreConfig = JobCoreConfiguration.newBuilder(jobClass.getName(), cron, shardingTotalCount).
+                shardingItemParameters(shardingItemParameters).jobParameter(jobParameters).build();
+        // 定义SIMPLE类型配置
+        SimpleJobConfiguration simpleJobConfig = new SimpleJobConfiguration(simpleCoreConfig, jobClass.getCanonicalName());
+        // 定义Lite作业根配置
+        LiteJobConfiguration simpleJobRootConfig = LiteJobConfiguration.newBuilder(simpleJobConfig).overwrite(true).build();
+        return simpleJobRootConfig;
 
     }
 }
